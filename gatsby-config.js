5dd884f50eb133d03763dca83f145abe58b22e46
Kel Kanhirun <kel@native.dev>
2024-02-18 10:20:49 -0500

    task: removes unused old config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const config = {
   siteMetadata: {
-    siteUrl: `https://www.dreamfight.io`,
+    siteUrl: `https://sechento.com`,
   },
   plugins: [
     {
@@ -21,51 +21,51 @@ const config = {
       },
       __key: "pages",
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: "DreamFight",
-        short_name: "DreamFight",
-        start_url: "/",
-        icon: "src/images/icon.png",
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
-        crossOrigin: `use-credentials`,
-      },
-    },
-    {
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "GTM-MHNRC8R",
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: "DreamFight",
+    //     short_name: "DreamFight",
+    //     start_url: "/",
+    //     icon: "src/images/icon.png",
+    //     // An optional attribute which provides support for CORS check.
+    //     // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+    //     // Any invalid keyword or empty string defaults to `anonymous`
+    //     crossOrigin: `use-credentials`,
+    //   },
+    // },
+    //{
+    //  resolve: "gatsby-plugin-google-tagmanager",
+    //  options: {
+    //    id: "GTM-MHNRC8R",
 
-        // Include GTM in development.
-        //
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
+    //    // Include GTM in development.
+    //    //
+    //    // Defaults to false meaning GTM will only be loaded in production.
+    //    includeInDevelopment: false,
 
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        //
-        // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
+    //    // datalayer to be set before GTM is loaded
+    //    // should be an object or a function that is executed in the browser
+    //    //
+    //    // Defaults to null
+    //    defaultDataLayer: { platform: "gatsby" },
 
-        // Specify optional GTM environment details.
-        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+    //    // Specify optional GTM environment details.
+    //    // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+    //    // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+    //    // dataLayerName: "YOUR_DATA_LAYER_NAME",
 
-        // Name of the event that is triggered
-        // on every Gatsby route change.
-        //
-        // Defaults to gatsby-route-change
-        routeChangeEventName: "dreamfight-route-change",
-        // Defaults to false
-        enableWebVitalsTracking: true,
-        // Defaults to https://www.googletagmanager.com
-        // selfHostedOrigin: "https://dreamfight.io",
-      },
-    },
+    //    // Name of the event that is triggered
+    //    // on every Gatsby route change.
+    //    //
+    //    // Defaults to gatsby-route-change
+    //    routeChangeEventName: "dreamfight-route-change",
+    //    // Defaults to false
+    //    enableWebVitalsTracking: true,
+    //    // Defaults to https://www.googletagmanager.com
+    //    // selfHostedOrigin: "https://dreamfight.io",
+    //  },
+    //},
     {
       resolve: 'gatsby-plugin-react-svg',
       options: {
